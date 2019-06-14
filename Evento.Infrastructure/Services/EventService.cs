0f8ed0b5eb498b8f0336286dda4e7d6ba8377b17
Rasti003 <przemyslaw.murzyn@outlook.com>
2019-06-14 22:09:47 +0200

    Repository Extensions

--- Evento.Infrastructure/Services/EventService.cs
+++ Evento.Infrastructure/Services/EventService.cs
@@ -5,6 +5,7 @@ using AutoMapper;
 using Evento.Core.Domain;
 using Evento.Core.Repositories;
 using Evento.Infrastructure.DTO;
+using Evento.Infrastructure.Extensions;
 
 
 namespace Evento.Infrastructure.Services
@@ -50,22 +51,15 @@ namespace Evento.Infrastructure.Services
 
         public async Task AddTicketAsync(Guid eventId, int amount, decimal price)
         {
-            var @event = await _eventRepository.GetAsync(eventId);
-            if (@event == null)
-            {
-                throw  new Exception($"Event with id: '{eventId}' does not exist.");
-            }
+            var @event = await _eventRepository.GetOrFailAsync(eventId);
+ 
             @event.AddTickets(amount, price);
             await _eventRepository.UpdateAsync(@event);
         }
 
         public async Task UpdateAsync(Guid id, string name, string description)
         {
-            var @event = await _eventRepository.GetAsync(id);
-            if (@event == null)
-            {
-                throw  new Exception($"Event with id: '{id}' does not exist.");
-            }
+            var @event = await _eventRepository.GetOrFailAsync(id);
             @event = await _eventRepository.GetAsync(name);
             if (@event != null)
             {
